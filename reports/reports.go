a693e6c0f10538ceff307280364f5de279e24b6d
Amit Ghadge <amitg.b14@gmail.com>
2017-11-14 17:10:57 +0530

    Modify Reports scripts.
    Signed-off-by: aghadge <aghadge@qualys.com>

--- reports/reports.go
+++ reports/reports.go
@@ -1,6 +1,33 @@
 package reports
 
+import (
+	"os"
+	"text/template"
+)
+
 // Report generates
 type Reports struct {
-	total int
+	TotalCount int
+	Pass int
+	TestName []map[string][]string
+}
+
+func (rep *Reports) FaildCount() int{
+	return rep.TotalCount - rep.Pass
+}
+
+const report_tesmp = `
+{{.TotalCount}} Requests		{{.Pass}} Passed	{{.FaildCount}} Failed
+---------------------------------------------------------------------------------
+{{range $test := .TestName}} {{range $key, $value := $test}}{{ index $test $key 1}} : {{$key}} => {{ index $test $key 0}}{{end}}
+{{end}}
+`
+
+func GeneratesReport(report *Reports) {
+
+	tmpl, err := template.New("test").Parse(report_tesmp)
+	if err != nil { panic(err) }
+	err = tmpl.Execute(os.Stdout, report)
+	if err != nil { panic(err) }
+
 }
